e4e14c79044eb88b8f8f72177db59072b7cf8d51
kermany <kermany>
2022-10-17 20:13:45 +0000

    updating notebooks

---
+++ README.py
@@ -0,0 +1,51 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC <img src=" https://hls-eng-data-public.s3.amazonaws.com/img/Databricks_HLS.png" width=700> 
+# MAGIC </div>
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Social Determinants of Health (SDH)
+# MAGIC Many aspects of a person's life style and status can impact health outcomes. Multiple studies suggest that Social Determinents of Health (SDH) account for between 30-55% of health outcomes. 
+# MAGIC [The WHO defines SDH as]((https://www.who.int/health-topics/social-determinants-of-health#tab=tab_1):
+# MAGIC >.. the non-medical factors that influence health outcomes. They are the conditions in which people are born, <br>
+# MAGIC > grow, work, live, and age, and the wider set of forces and systems shaping the conditions of daily life. <br>
+# MAGIC > These forces and systems include economic policies and systems, development agendas, social norms, social <br>
+# MAGIC > policies and political systems.
+# MAGIC 
+# MAGIC <img src='https://www.uclahealth.org/sites/default/files/styles/max_width_012000_480/public/images/SOCIAL%20DETERMINANTS%20OF%20HEALTH%20GRAPHIC.png' width=25%/>
+# MAGIC 
+# MAGIC 
+# MAGIC Correlation between SDH and health outcomes is very clear: the lower the socioeconomic position, the worse the health, which in turn creates a negative feedback loop (poor health resulting in poor socioeconomic status) which results in widening the gap even more. 
+# MAGIC 
+# MAGIC There are many public sources of SDH data with different levels of granularity (country level, state/province, county, or zipcode/postal code level) that can be used in analysis of the impact of SDH on health outcomes. One of the main challenegs for data analysis is finding the right data source and data cleaning. 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Access SDH data via Delta Share
+# MAGIC In this solution accelerator, we use pre-processed and cleansed tables that have been made available by [Rearc](rearc.io) via [delta sharing protocol](https://www.databricks.com/blog/2021/05/26/introducing-delta-sharing-an-open-protocol-for-secure-data-sharing.html). We explore income, healthcare, education and other aspects affect counties vaccinations rates for COVID-19. 
+# MAGIC 
+# MAGIC Delta sharing allows us to offload the "bronze to silver" data prep step to a data provider or internal data team, beginning our analysis with "silver" or "gold" data.
+# MAGIC Using these data, we train a machine learning model to predict vaccination rates based on different SDH features and then use [SHAP](https://shap.readthedocs.io/en/latest/example_notebooks/overviews/An%20introduction%20to%20explainable%20AI%20with%20Shapley%20values.html) to offer insights into different factors impacting vaccination rates.
+# MAGIC 
+# MAGIC ![](https://databricks.com/wp-content/uploads/2022/03/delta-lake-medallion-architecture-2.jpeg)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## License
+# MAGIC Copyright / License info of the notebook. Copyright [2021] the Notebook Authors.  The source in this notebook is provided subject to the  the [Databricks License] (https://databricks.com/db-license-source).  All included or referenced third party libraries are subject to the licenses set forth below.
+# MAGIC 
+# MAGIC |Library Name|Library License|Library License URL|Library Source URL| 
+# MAGIC | :-: | :-:| :-: | :-:|
+# MAGIC |Pandas |BSD 3-Clause License| https://github.com/pandas-dev/pandas/blob/master/LICENSE | https://github.com/pandas-dev/pandas|
+# MAGIC |Numpy |BSD 3-Clause License| https://github.com/numpy/numpy/blob/main/LICENSE.txt | https://github.com/numpy/numpy|
+# MAGIC |Apache Spark |Apache License 2.0| https://github.com/apache/spark/blob/master/LICENSE | https://github.com/apache/spark/tree/master/python/pyspark|
+# MAGIC |SHAP| MIT | https://github.com/slundberg/shap/blob/master/LICENSE | https://github.com/slundberg/shap/|
+# MAGIC 
+# MAGIC |Author|
+# MAGIC |-|
+# MAGIC |Databricks Inc.|
---
+++ SDH-vaccination-rates.py
@@ -0,0 +1,302 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Create a SDOH Database based on publically available data
+# MAGIC There are a number of datasets from various sources we used to explore their affects on vacination rates of COVID-19. 
+# MAGIC The datasets include population, income, education, poverty, health traits and more at a county level
+# MAGIC Many of the files needed some level of data cleansing before they can be used. We applied these transformations and created the silver layer and share cleansed tables using deltashare.
+# MAGIC 
+# MAGIC Having access to the data, now we simply create a database based on these cleaned datasets and start with our analysis.
+# MAGIC 
+# MAGIC As an example of the pre-processing steps, to create the `Income` table, we first clean up the FIPS field as part initial importing:
+# MAGIC ```
+# MAGIC from pyspark.sql.functions import regexp_replace, trim
+# MAGIC from pyspark.sql.types import  StructType   
+# MAGIC import json
+# MAGIC new_schema = StructType.fromJson(json.loads(incomeSchema))
+# MAGIC 
+# MAGIC dfIncome = spark.read.csv(storageBase + "/all_counties_income.csv", header=True, schema=new_schema) \
+# MAGIC   .filter("Description ='Per capita personal income (dollars) 2/'") \
+# MAGIC   .withColumn("GeoFIPS",regexp_replace('GeoFIPS','"',"")) \
+# MAGIC   .withColumn("GeoFIPS",regexp_replace('GeoFIPS',' ',""))
+# MAGIC ```
+# MAGIC 
+# MAGIC Similarly we created education, health, pverty and vaccination tables in the bronze layer.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ```%sh
+# MAGIC mkdir -p /dbfs/FileSystem/tmp/delta_share/
+# MAGIC cp config/rearc_hls_data.share /dbfs/FileSystem/tmp/delta_share/```
+
+# COMMAND ----------
+
+# MAGIC %sh pip install delta-sharing s3fs
+
+# COMMAND ----------
+
+import delta_sharing
+share_file_path = "s3://hls-eng-data-public/delta_share/rearc_hls_data.share"
+client = delta_sharing.SharingClient(share_file_path)
+shared_tables = client.list_all_tables()
+
+# COMMAND ----------
+
+[t for t in shared_tables if t.schema =='hls_covid19_usa']
+
+# COMMAND ----------
+
+# DBTITLE 1,setup deltashare
+share_file_path = "/FileSystem/tmp/delta_share/rearc_hls_data.share"
+
+dataset_urls = {
+  "bronze_income":f"{share_file_path}#rearc_databricks_hls_share.hls_sdoh.bronze_income",
+  "silver_poverty":f"{share_file_path}#rearc_databricks_hls_share.hls_sdoh.poverty_county",
+  "silver_education":f"{share_file_path}#rearc_databricks_hls_share.hls_sdoh.education_county",
+  "silver_health_stats":f"{share_file_path}#rearc_databricks_hls_share.hls_sdoh.health_stats_county",
+  "silver_race":f"{share_file_path}#rearc_databricks_hls_share.hls_sdoh.race_county",
+  "silver_vaccinations":f"{share_file_path}#rearc_databricks_hls_share.hls_covid19_usa.vaccinations_county_utd",
+}
+
+# Create schema
+spark.sql("CREATE SCHEMA IF NOT EXISTS sdoh")
+
+# Add tables
+for ds, url in dataset_urls.items():
+  spark.sql(f"CREATE TABLE IF NOT EXISTS sdoh.{ds} USING deltaSharing LOCATION '{url}'")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use sdoh
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show tables
+
+# COMMAND ----------
+
+# DBTITLE 1,Education Status
+# MAGIC %sql
+# MAGIC select * from silver_education
+
+# COMMAND ----------
+
+# DBTITLE 1,Health Stats
+# MAGIC %sql
+# MAGIC select * from silver_health_stats
+
+# COMMAND ----------
+
+# DBTITLE 1,vaccinations
+# MAGIC %sql
+# MAGIC select * from silver_vaccinations
+
+# COMMAND ----------
+
+# DBTITLE 1,Vaccination rates for 12 and older across counties 
+from urllib.request import urlopen
+import json
+import pandas as pd
+import plotly.express as px
+import numpy as np
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+_pdf = sql('select fips,avg(Series_Complete_12PlusPop_Pct) as vaccination_rate  from silver_vaccinations group by fips').toPandas()
+fig = px.choropleth(_pdf, geojson=counties, locations='fips', color='vaccination_rate',
+                           color_continuous_scale="Viridis",
+                           scope="usa",
+                           labels={'vaccination_rate':'vaccination_rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # 2. Build the ML model
+# MAGIC Now we proceed to create a dataset for downstream analysis using ML. 
+# MAGIC The target value to predict is vaccination rate for people 12 years and older which is `Series_Complete_12PlusPop_Pct`
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Create training data
+# MAGIC Here, we also need population densicty data. In this case, we directly read the `csv` files and register the resulting dataset as a view.
+
+# COMMAND ----------
+
+# DBTITLE 1,adding population density
+spark.read.csv('wasb://data@sdohworkshop.blob.core.windows.net/sdoh/Population_Density_By_County.csv', header=True, inferSchema=True).createOrReplaceTempView('population_density')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from population_density
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Now that we have all the data, we create the dataset needed for our downstream analysis.
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use sdoh;
+# MAGIC create or replace temp view vaccine_data_pct
+# MAGIC as
+# MAGIC select v.fips, Recip_County, Recip_State, ifnull(Series_Complete_12PlusPop_Pct, Series_Complete_Pop_Pct) Series_Complete_12PlusPop_Pct, pd.Density_per_square_mile_of_land_area population_density, r.County_Population, round((r.County_Population - r.White_population) / r.County_Population,3) * 100 Minoirity_Population_Pct, 
+# MAGIC i.`2019` income, p.All_Ages_in_Poverty_Percent, round(e.25PlusHS / r.County_Population,2) * 100 25PlusHSPct, round(e.25PlusAssociate / r.County_Population,2) * 100 25PlusAssociatePct, h.SmokingPct, h.ObesityPct, h.HeartDiseasePct, h.CancerPct, h.NoHealthInsPct,h.AsthmaPct
+# MAGIC from silver_race r join sdoh.silver_vaccinations v on(r.fips = v.fips)
+# MAGIC join bronze_income i on(i.geofips = v.fips)
+# MAGIC join silver_poverty p on (p.fips = v.fips)
+# MAGIC join silver_education e on (e.fips = v.fips)
+# MAGIC join silver_health_stats h on(h.locationid = v.fips)
+# MAGIC join population_density pd on (pd.GCT_STUBtarget_geo_id2 = v.fips)
+
+# COMMAND ----------
+
+# DBTITLE 1,View the dataset
+# MAGIC %sql
+# MAGIC select * from vaccine_data_pct
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select Recip_State,
+# MAGIC   avg(Series_Complete_12PlusPop_Pct) as Series_Complete_12PlusPop_Pct,
+# MAGIC   avg(population_density),
+# MAGIC   sum(County_Population),
+# MAGIC   avg(Minoirity_Population_Pct),
+# MAGIC   percentile(income,0.5),
+# MAGIC   avg(All_Ages_in_Poverty_Percent),
+# MAGIC   avg(25PlusHSPct),
+# MAGIC   avg(25PlusAssociatePct),
+# MAGIC   avg(SmokingPct),
+# MAGIC   avg(ObesityPct),
+# MAGIC   avg(HeartDiseasePct),
+# MAGIC   avg(CancerPct),
+# MAGIC   avg(NoHealthInsPct),
+# MAGIC   avg(AsthmaPct)
+# MAGIC from vaccine_data_pct
+# MAGIC group by Recip_State
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Now we create a pandas data frame which will used by the ML framework
+
+# COMMAND ----------
+
+parsed_pd = spark.table("vaccine_data_pct").toPandas().dropna(subset=['Series_Complete_12PlusPop_Pct'])
+parsed_pd.set_index("fips")
+
+X = parsed_pd.drop(["Series_Complete_12PlusPop_Pct", "fips","Recip_County","Recip_State","County_Population"], axis=1)
+y = parsed_pd["Series_Complete_12PlusPop_Pct"]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Linear Regression with XG Boost
+# MAGIC Now, we use XG Boost to train a linear regression model and use MLFlow autolog to track model performance
+
+# COMMAND ----------
+
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from math import sqrt
+import numpy as np
+import pandas as pd
+import shap
+import mlflow
+
+mlflow.autolog()
+
+# COMMAND ----------
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=0)
+
+max_depth=3
+learning_rate=.1
+reg_alpha=.1
+
+# We use n_estimators=500 for to accelerate the training, in practice you may want to use higher values for more accurare results
+xgb_regressor = XGBRegressor(objective='reg:squarederror', max_depth=max_depth, learning_rate=learning_rate, reg_alpha=reg_alpha, n_estimators=500, importance_type='total_gain', random_state=0)
+
+xgb_model = xgb_regressor.fit(X_train, y_train, eval_set=[(X_test, y_test)],\
+                              eval_metric='rmse', early_stopping_rounds=25)
+
+n_estimators = len(xgb_model.evals_result()['validation_0']['rmse'])
+y_pred = xgb_model.predict(X_test)
+mae = mean_absolute_error(y_pred, y_test)
+rmse = sqrt(mean_squared_error(y_pred, y_test))
+displayHTML(f"mae =<b>{mae}</b> and rsme: <b>{rmse}</b>")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Model Explainability and Feature Importance
+# MAGIC Now, to explore the impact of SDOH factors affecting vacination rates, we use SHAP values. 
+# MAGIC For an introduction to SHAP see [this blog](https://towardsdatascience.com/shap-explained-the-way-i-wish-someone-explained-it-to-me-ab81cc69ef30#:~:text=In%20a%20nutshell%2C%20SHAP%20values,answer%20the%20%E2%80%9Chow%20much%E2%80%9D').
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Add FIPS Codes to SHAP Values
+
+# COMMAND ----------
+
+df = pd.DataFrame(shap_values, columns=X.columns) 
+df['fips'] =spark.sql("select fips from vaccine_data_pct order by fips").toPandas()
+dfShaps = spark.createDataFrame(df)
+dfShaps.createOrReplaceTempView("shap")
+
+# COMMAND ----------
+
+sql('select * from shap').display()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Pivot the columns back to rows to make reporting easier and add to the database
+
+# COMMAND ----------
+
+usa_model_county_vaccine_shap_df = sql("""
+select fips, stack(12,'Minority_Population_Pct',Minoirity_Population_Pct,'income', income, '25PlusHSPct', 25PlusHSPct,
+'All_Ages_in_Poverty_Percent',All_Ages_in_Poverty_Percent,'population_density', population_density, '25PlusAssociatePct', 25PlusAssociatePct, 
+'SmokingPct',SmokingPct, 
+'ObesityPct', ObesityPct, 
+'HeartDiseasePct', HeartDiseasePct, 
+'CancerPct', CancerPct, 
+'NoHealthInsPct', NoHealthInsPct,
+'AsthmaPct', AsthmaPct
+)  
+as (factor, value)
+from shap
+""")
+display(usa_model_county_vaccine_shap_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We note that the values across the USA income, smoking and health insurance are the leading factors of vaccation rates:
+
+# COMMAND ----------
+
+mean_abs_shap = np.absolute(shap_values).mean(axis=0).tolist()
+display(spark.createDataFrame(sorted(list(zip(mean_abs_shap, X.columns)), reverse=True)[:6], ["Mean |SHAP|", "Column"]))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Recap
+# MAGIC In this notebook we quickly and easily:
+# MAGIC   * Brought in pre-processed data from a varitey of datasources using deltasharing.
+# MAGIC   * We then created a dataset of all SDH alogside vacciantion rates for each county in the US
+# MAGIC   * Trained a regression model to predict vaccination rates based on SDH and county-level geographic information such as population density and size
+# MAGIC   * We used SHAP to explain the impact of each SDH factor on vaccination rates
