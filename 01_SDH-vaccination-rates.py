15e0576ec31d98ee132a62e16291f755e4bf0f5d
nicole@databricks.com <nicole@databricks.com>
2022-10-17 23:40:05 +0000

    Update share_file_path to work with both sharing client and table creation APIs

--- 01_SDH-vaccination-rates.py
+++ 01_SDH-vaccination-rates.py
@@ -36,8 +36,8 @@
 # DBTITLE 1,retrieve share credentials file 
 import delta_sharing
 dbutils.fs.cp('s3://hls-eng-data-public/delta_share/rearc_hls_data.share','/tmp/')
-share_file_path = "dbfs:/tmp/rearc_hls_data.share"
-client = delta_sharing.SharingClient(share_file_path)
+share_file_path = "/tmp/rearc_hls_data.share"
+client = delta_sharing.SharingClient(f"/dbfs{share_file_path}")
 shared_tables = client.list_all_tables()
 
 # COMMAND ----------
